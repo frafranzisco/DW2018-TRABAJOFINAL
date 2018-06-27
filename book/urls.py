780213d060cd91e3fa88cf31c4551e2b06697cad
paanchoz <franciscoparrahuentecura@gmail.com>
2018-06-27 00:15:20 -0400

    Versión final Proyecto

--- book/urls.py
+++ book/urls.py
@@ -2,19 +2,14 @@ from django.urls import path
 from book import views
 
 urlpatterns = [
-    
-	path('inicio/', views.inicio, name="inicio"),
+    #CRUD BOOKUSER
+	path('index/', views.index, name="index"),
 	path('list_book', views.list_book, name="list_book"),
     path('add_book/', views.add_book, name="add_book"),
-	# path('update_book/<str:id>', views.update_book, name="update_book"),
-	# path('delete_book/<str:id>', views.delete_book, name="delete_book"),
-	path('detail_book/<str:id>', views.detail_book, name="detail_book"),
-	#HAY QUE HACER FUNCIONAR EL BOTÓN DE AGREGAR LIBRO EN LIST_BOOK
-	#path('index',views.index,name="index"),
-	path('genre/',views.add_genre,name="genre"),
-	#CRUD BOOKUSER
-	path('add_user/',views.add_user,name='user_add'),
-    path('list_user/',views.list_user,name='user_list'),
-    path('edit_user/<str:user_run>',views.edit_user,name='user_edit'),
-    path('remove_user/<str:user_run>',views.remove_user,name='user_remove'),
+    path('edit_book/<str:id>', views.edit_book, name="edit_book"),
+    path('detail_book/<str:id>', views.detail_book, name="detail_book"),
+    path('view_book/<str:id>', views.view_book, name="view_book"),
+	path('delete_book/<str:id>', views.delete_book, name="delete_book"),
+	path('view_sales/', views.view_sales, name='view_sales'),
+	path('view_both/', views.view_both, name='view_both'),
 ]
